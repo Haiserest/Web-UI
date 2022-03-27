7a4ac753721c5e285f9ffd6c5ac26a8444ba232f
Haiserest <blach2542@hotmail.com>
2022-03-27 18:07:08 +0700

    API

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { MainComponent } from './page/main/main.component'
 import { AboutUsComponent } from './page/about-us/about-us.component'
+import { MemberComponent } from './page/member/member.component';
 
 const routes: Routes = [
   { path: '', component: MainComponent},
   { path: 'home', component: MainComponent},
+  { path: 'member', component: MemberComponent},
   { path: 'aboutus', component: AboutUsComponent}
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,33 +1,43 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { HttpClientModule } from '@angular/common/http'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainComponent } from './page/main/main.component';
 import { HeaderComponent } from './header/header.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AboutUsComponent } from './page/about-us/about-us.component';
+import { MemberComponent } from './page/member/member.component'
+
+import { MemberServiceService } from './page/member/member-service.service'
 
 import { MatIconModule } from '@angular/material/icon'
 import { MatToolbarModule } from '@angular/material/toolbar'
 import { MatButtonModule } from '@angular/material/button';
-import { AboutUsComponent } from './page/about-us/about-us.component'
+import { MatTableModule } from '@angular/material/table';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainComponent,
     HeaderComponent,
-    AboutUsComponent
+    AboutUsComponent,
+    MemberComponent
   ],
   imports: [
+    HttpClientModule,
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatIconModule,
     MatToolbarModule,
-    MatButtonModule
+    MatButtonModule,
+    MatTableModule
   ],
-  providers: [],
+  providers: [
+    MemberServiceService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
